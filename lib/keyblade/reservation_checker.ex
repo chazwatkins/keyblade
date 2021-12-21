f57c8c3aa60cb13ae16a61117225d0d1af01745b
Chaz Watkins <chazwatkins@hey.com>
2021-12-21 11:48:52 -0600

    Update log message with restaurant name

--- lib/keyblade/reservation_checker.ex
+++ lib/keyblade/reservation_checker.ex
@@ -22,16 +22,28 @@ defmodule Keyblade.ReservationChecker do
       |> Map.put(:queries, [])
       |> Map.put(:reservation_times, [])
 
-    Logger.info("Checking reservations for #{search_params.restaurant_id}")
+    Logger.info(
+      "Checking reservations for #{search_params.restaurant_name} - User: #{search_params.notify_number}"
+    )
+
     Keyblade.check_for_available_reservations(%DisneyWorld{}, search_params)
-    Logger.info("Finished checking reservations for #{search_params.restaurant_id}")
+
+    Logger.info(
+      "Finished checking reservations for #{search_params.restaurant_name} - User: #{search_params.notify_number}"
+    )
 
     schedule_work(search_params)
     {:noreply, search_params}
   end
 
-  def schedule_work(%SearchParams{interval: interval, restaurant_id: restaurant_id}) do
-    Logger.info("Next reservation check scheduled for #{restaurant_id}")
+  def schedule_work(%SearchParams{
+        interval: interval,
+        restaurant_name: restaurant_name,
+        notify_number: notify_number
+      }) do
+    Logger.info(
+      "Next reservation check scheduled for #{restaurant_name} - User: #{notify_number}"
+    )
 
     next_interval_timer = apply(:timer, interval.measure, [interval.length])
     Process.send_after(self(), :check_reservations, next_interval_timer)
